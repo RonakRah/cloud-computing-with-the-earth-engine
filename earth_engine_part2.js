1c9d424f3b77a9752a15ca4acb2b625f1bc26005
Ronak Rah <ronakrahmati25@gmail.com>
2022-10-27 16:17:53 +0200

    the second class Computations using Images is done

--- earth_engine_part2.js
+++ earth_engine_part2.js
@@ -23,7 +23,7 @@ Map.addLayer(slope, {min: 0, max :60}, 'slope');
 // 3-  display the result by add layer 
 
 // 1
-var aspect = ee.Terrain.aspect(srtm);
+var aspect = ee.Terrain.aspect(srtm); // with this aspect fucntion you will get the N S W E
 
 // 2
 var sinImage = aspect.divide(180).multiply(Math.PI).sin();
@@ -70,4 +70,100 @@ print('SRTM scale in meters', scale);
  //4
  var mean = meanDictionary.get("elevation"); // getting the mean value for variable elevation
  print("mean elevation" , mean);
+
+
+
+// calculating min max and median individually//
+
+//A) min with 80 resolution
+var minDictionary = srtmImage.reduceRegion(
+    {
+      reducer : ee.Reducer.min(),// the reducer parameter specifies the type of statistic to compute 
+        geometry : polygon, // the geometry parameter specifies the region in which to compute the statistic
+        scale : 80 ,//The scale parameter is the pixel size in meters to use "specify it from zoom level of the image"
+        bestEffort : true // it make sure just the area of interest is calculated
+
+    }
+)
+ print("minDictionary" , minDictionary);
+ var min = minDictionary.get("elevation"); // getting the mean value for variable elevation
+ print("min elevation" , min);
+
+//B) max in 60 resolution 
+var maxDictionary = srtmImage.reduceRegion(
+    {
+      reducer : ee.Reducer.max(),// the reducer parameter specifies the type of statistic to compute 
+        geometry : polygon, // the geometry parameter specifies the region in which to compute the statistic
+        scale : 60 ,//The scale parameter is the pixel size in meters to use "specify it from zoom level of the image"
+        bestEffort : true // it make sure just the area of interest is calculated
+
+    }
+)
+ print("maxDictionary" , maxDictionary);
+ var max = maxDictionary.get("elevation"); // getting the mean value for variable elevation
+ print("max elevation" , max);
+
+
+//c) median in 70 resolution
+var medianDictionary = srtmImage.reduceRegion(
+    {
+      reducer : ee.Reducer.median(),// the reducer parameter specifies the type of statistic to compute 
+        geometry : polygon, // the geometry parameter specifies the region in which to compute the statistic
+        scale : 70 ,//The scale parameter is the pixel size in meters to use "specify it from zoom level of the image"
+        bestEffort : true // it make sure just the area of interest is calculated
+
+    }
+)
+ print("medianDictionary" , medianDictionary);
+
+// calculating SD and mean in one go by combine function
+// defining the reducers
+
+var reducers = ee.Reducer.mean().combine({
+  reducer2: ee.Reducer.stdDev(),
+  sharedInputs: true,
+  
+});
+
+// put the reducers inside the reduceRegion().
+var meanStdDictionary = srtmImage.reduceRegion({
+  reducer: reducers,
+   scale : 50,
+  bestEffort: true,
+});
+
+// Display the dictionary of band means and SDs.
+print("meanStdDictionary",meanStdDictionary);
+
+
+
+
+
+ // How to calculate aspect,slope and hillshade individually
+//A) Calculate aspect. Units are degrees where 0=N, 90=E, 180=S, 270=W & display.
+var aspect = ee.Terrain.aspect(srtmImage);
+print("aspect of srtm image", aspect);
+Map.addLayer(aspect, {min: 0, max: 359.99}, 'aspect');
+
+//B) Calculate slope. Units are degrees, range is [0,90) & display.
+var slopeSrtmImage = ee.Terrain.slope(srtmImage);
+print("slope of srtm image",slopeSrtmImage);
+Map.addLayer(slopeSrtmImage, {min: 0, max: 89.99}, 'slope');
+
+//C) calculate hillshade of the mage
+var elevation = srtmImage.select("elevation");
+print("elevation of the image",elevation);
+
+var exaggeration = 20;
+var hillshade = ee.Terrain.hillshade(elevation.multiply(exaggeration));
+print("hillshade of elevation of the image",hillshade);
+Map.addLayer(hillshade, null, 'Hillshade individually');
+
+
+// get slope ,aspect and hillshade of the image in one go
+var terrain = ee.Terrain.products(srtmImage);
+print("terrain of srtm image", terrain);
+print('ee.Terrain.products bands', terrain.bandNames());
+Map.addLayer(terrain.select('hillshade'), {min: 0, max: 255}, 'Hillshade');
+
 
